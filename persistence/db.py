96385f932b2f3a177c71fb336d738a38585e62df
rosagabriela <rosagabrielapina@gmail.com>
2025-11-11 11:28:21 -0700

    se creo archivo dp.py

---
+++ persistence/db.py
@@ -0,0 +1,10 @@
+import pymysql
+
+def get_connection():
+    return pymysql.connect(
+        host='127.0.0.1', 
+        port=3306,
+        user='root',
+        password='250597Pi',
+        database='cheems'
+    )
