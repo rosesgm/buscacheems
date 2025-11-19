5c5de0f71f0e5d4d255f89b13e37444af7492f07
rosagabriela <rosagabrielapina@gmail.com>
2025-11-19 08:26:40 -0700

    agregamos phrase en la ruta winner

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, jsonify
+from entities.winner import Winner
 
 app = Flask(__name__)
 # Esta sera la ruta index (de la pagina principal)
@@ -8,6 +8,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/winner', methods=['POST'])
+def save_winner():
+    data = request.get_json()
+
+    winner = Winner(id=0, name=data['name'], email=data['email'], phrase=data['phrase'])
+
+    winner.save()
+    if winner.id != 0:
+        return jsonify({"success": True, "id": winner.id}), 201
+    else:
+        return jsonify({"success": False}), 500
 
 
 if __name__ == '__main__':
