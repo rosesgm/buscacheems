d85e858aa5430b119e537deecee007354fd26605
rosagabriela <rosagabrielapina@gmail.com>
2025-11-19 08:28:58 -0700

    no me acuerdo que hicimos

--- persistence/db.py
+++ persistence/db.py
@@ -1,8 +1,9 @@
 import pymysql
 
+
 def get_connection():
     return pymysql.connect(
-        host='127.0.0.1', 
+        host='127.0.0.1',
         port=3306,
         user='root',
         password='250597Pi',
