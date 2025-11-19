3c8406e71430c48a3baec7394b1652f19964a570
rosagabriela <rosagabrielapina@gmail.com>
2025-11-19 08:28:46 -0700

    agregamos un parametro phrase en el construtor de la clase winner

--- entities/winner.py
+++ entities/winner.py
@@ -3,13 +3,25 @@ from persistence.db import get_connection
 
 class Winner:
 
-    def __init__(self, id, name, email):
+    def __init__(self, id, name, email, phrase):
         self.id = id
         self.name = name
         self.email = email
-
+        self.phrase=phrase
     def save(self):
-        connection = get_connection()
-        cursor = connection.cursor()
-        query = "INSERT INTO winners (name, email) VALUES (%s, %s)"
-        cursor.execute(query, (self.name, self.email))
+        try:
+            connection = get_connection()
+            cursor = connection.cursor()
+
+            #consulta parametrizada
+            query = "INSERT INTO winners (name, email, phrase) VALUES (%s, %s, %s)"
+            cursor.execute(query, (self.name, self.email, self.phrase))
+            connection.commit()
+            self.id = cursor.lastrowid
+            return self.id
+        except Exception as ex: 
+            print("Error al guardar registro: ", ex)
+            return 0
+        finally:
+            cursor.close()
+            connection.close()
